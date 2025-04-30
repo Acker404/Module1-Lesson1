dd12466485937d5aa16ad44e543dfe018aa5a095
Acker404 <mark921202@gmail.com>
2025-04-30 21:51:36 +0800

    加入專案檔案。

---
+++ Module1-Lesson1/firstApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate void MyDelegate(string message);
+
+namespace Module1_Lesson1
+{
+    public class firstApp
+    {
+        public static void Main(string[] args)
+        {
+            MyDelegate delegateInstance = Console.WriteLine;
+            delegateInstance("Hello, delagates");
+        }
+    }
+}
+
---
+++ Module1-Lesson1/secondApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_Lesson1
+{
+    public class secondApp
+    {
+        public static void Main(string[] args)
+        {
+            Button b1 = new Button();
+            b1.Click += Button_Click;
+            b1.DunnyClick();
+        }
+        static void Button_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked!");
+        }
+    }
+    public class  Button
+    {
+        public event EventHandler Click;
+        public void DunnyClick()
+        {
+            Click?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
